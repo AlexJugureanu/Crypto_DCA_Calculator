71449774033591d7f6c11a09be0dd0ccbd101f90
AlexJugureanu <ajugureanu@gmail.com>
2025-08-06 19:08:19 +0300

    Created basic views

---
+++ Crypto_DCA_Calculator_MobileApp/Views/DcaSimulatorPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Crypto_DCA_Calculator_MobileApp.Views;
+
+public partial class DcaSimulatorPage : ContentPage
+{
+	public DcaSimulatorPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Crypto_DCA_Calculator_MobileApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Crypto_DCA_Calculator_MobileApp.Views;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
