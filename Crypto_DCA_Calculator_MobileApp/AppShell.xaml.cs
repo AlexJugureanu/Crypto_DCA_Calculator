234b1253ece4f67e10ba9573c0439bc880e7e52e
AlexJugureanu <ajugureanu@gmail.com>
2025-08-06 19:01:07 +0300

    Add project files.

---
+++ Crypto_DCA_Calculator_MobileApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Crypto_DCA_Calculator_MobileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Crypto_DCA_Calculator_MobileApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Crypto_DCA_Calculator_MobileApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
