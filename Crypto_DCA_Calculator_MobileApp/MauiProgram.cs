729ba192b15e14c79b847911d468b26b3be8b015
AlexJugureanu <ajugureanu@gmail.com>
2025-08-07 19:48:52 +0300

    Started working on the DCA simulator page.

--- Crypto_DCA_Calculator_MobileApp/MauiProgram.cs
+++ Crypto_DCA_Calculator_MobileApp/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿using Crypto_DCA_Calculator_MobileApp.Services;
 using Microsoft.Extensions.Logging;
+using Syncfusion.Licensing;
+using Syncfusion.Maui.Core.Hosting;
 
 namespace Crypto_DCA_Calculator_MobileApp
 {
@@ -9,12 +11,15 @@ namespace Crypto_DCA_Calculator_MobileApp
         {
             var builder = MauiApp.CreateBuilder();
 
-            builder.Services.AddTransient<IDcaCalculatorService, DcaCalculatorService>();
+			SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JEaF1cWWhBYVJyWmFZfVtgdVdMY15bRndPIiBoS35Rc0VrWXhfcndXRGFYV0x0VEFd\r\n");
+
+			builder.Services.AddTransient<IDcaCalculatorService, DcaCalculatorService>();
 			builder.Services.AddTransient<ICryptoDataService, CryptoDataService>();
 
 			builder
 				.UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
+				.ConfigureSyncfusionCore()
+				.ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
--- Crypto_DCA_Calculator_MobileApp/Views/DcaSimulatorPage.xaml.cs
+++ Crypto_DCA_Calculator_MobileApp/Views/DcaSimulatorPage.xaml.cs
@@ -1,9 +1,50 @@
+using System.ComponentModel;
+
 namespace Crypto_DCA_Calculator_MobileApp.Views;
 
-public partial class DcaSimulatorPage : ContentPage
+public partial class DcaSimulatorPage : ContentPage, INotifyPropertyChanged
 {
+	private List<string> _cryptoCurrencies = [ "Bitcoin (BTC)", "Ethereum (ETH)", "Solana (SOL)" ];
+
+	public List<string> CryptoCurrencies
+	{
+		get => _cryptoCurrencies;
+		set
+		{
+			if (_cryptoCurrencies != value)
+			{
+				_cryptoCurrencies = value;
+				OnPropertyChanged(nameof(CryptoCurrencies));
+			}
+		}
+	}
+
+	private string _selectedCryptoCurrency;
+
+	public string SelectedCryptoCurrency
+	{
+		get => _selectedCryptoCurrency;
+		set
+		{
+			if (_selectedCryptoCurrency != value)
+			{
+				_selectedCryptoCurrency = value;
+				OnPropertyChanged(nameof(SelectedCryptoCurrency));
+			}
+		}
+	}
+
 	public DcaSimulatorPage()
 	{
 		InitializeComponent();
+
+		BindingContext = this;
+
+		SelectedCryptoCurrency = _cryptoCurrencies[0];
 	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+	protected void OnPropertyChanged(string propertyName) =>
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
 }
