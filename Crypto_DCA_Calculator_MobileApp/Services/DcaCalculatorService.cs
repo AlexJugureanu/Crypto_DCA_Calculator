d5c3a7ac85f7f09b30d0326d8066b0804eb37d61
AlexJugureanu <ajugureanu@gmail.com>
2025-08-10 13:00:50 +0300

    Cleaned up the basic calculator service.

--- Crypto_DCA_Calculator_MobileApp/Services/DcaCalculatorService.cs
+++ Crypto_DCA_Calculator_MobileApp/Services/DcaCalculatorService.cs
@@ -4,41 +4,18 @@ namespace Crypto_DCA_Calculator_MobileApp.Services;
 
 public interface IDcaCalculatorService
 {
-	Task<(DcaResult, List<DcaResultForUser>)> CalculateDca(DcaSimulationInput input);
+	Task<List<DcaResultForUser>> CalculateDca(DcaSimulationInput input);
 }
 
 public class DcaCalculatorService(ICryptoDataService cryptoDataService) : IDcaCalculatorService
 {
-	public async Task<(DcaResult, List<DcaResultForUser>)> CalculateDca(DcaSimulationInput input)
+	public async Task<List<DcaResultForUser>> CalculateDca(DcaSimulationInput input)
 	{
 		var cryptoPrices = await cryptoDataService.GetMonthlyDcaPricesAsync(
 			input.CryptoCoin.Id,
 			new DateTimeOffset(input.StartDate).ToUnixTimeSeconds(),
 			new DateTimeOffset(input.EndDate).ToUnixTimeSeconds(),
 			input.InvestDayOfTheMonth);
-
-		var totalNumberOfCryptosBought2 = 0.0;
-
-		foreach (var price in cryptoPrices)
-		{
-			totalNumberOfCryptosBought2 += input.MonthlyAmount / price.Value;
-		}
-
-		var numberOfMonths = GetNumberOfMonths(input.StartDate, input.EndDate);
-		//if (numberOfMonths <= 0)
-		//	return new (DcaResult, DcaResultForUser)();
-
-		var dcaResult = new DcaResult
-		{
-			InvestDayOfTheMonth = input.InvestDayOfTheMonth,
-			AmountInvested = input.MonthlyAmount * numberOfMonths,
-			ValueToday = totalNumberOfCryptosBought2 * cryptoPrices.FirstOrDefault(x => x.Key.Month == DateTime.Now.Month && x.Key.Year == DateTime.Now.Year).Value,
-			ProfitLoss = (totalNumberOfCryptosBought2 * cryptoPrices.FirstOrDefault(x => x.Key.Month == DateTime.Now.Month && x.Key.Year == DateTime.Now.Year).Value) - (input.MonthlyAmount * numberOfMonths)
-		};
-
-		var currentValue = (totalNumberOfCryptosBought2 * cryptoPrices.FirstOrDefault(x => x.Key.Month == DateTime.Now.Month && x.Key.Year == DateTime.Now.Year).Value);
-		var amountInvested = input.MonthlyAmount * numberOfMonths;
-		var roi = (currentValue - amountInvested) / amountInvested * 100;
 
 		var dcaResultForUserList = new List<DcaResultForUser>();
 		var index = 1;
@@ -62,17 +39,6 @@ public class DcaCalculatorService(ICryptoDataService cryptoDataService) : IDcaCa
 			dcaResultForUserList.Add(dcaResultForUser);
 		}
 
-		return (dcaResult, dcaResultForUserList);
-	}
-
-	private static int GetNumberOfMonths(DateTime startDate, DateTime endDate)
-	{
-		int months = 0;
-		while (startDate < endDate)
-		{
-			startDate = startDate.AddMonths(1);
-			months++;
-		}
-		return months;
+		return dcaResultForUserList;
 	}
 }
